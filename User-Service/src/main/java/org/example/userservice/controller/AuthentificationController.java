08fcbbd18ce67fb77d3597e3171fe9f7a143e8be
KAMA7775 <kalia.abdusamatova@gmail.com>
2025-03-07 15:42:29 +0600

    registration working

--- User-Service/src/main/java/org/example/userservice/controller/AuthentificationController.java
+++ User-Service/src/main/java/org/example/userservice/controller/AuthentificationController.java
@@ -1,29 +1,34 @@
 package org.example.userservice.controller;
 
+import org.example.userservice.dto.AuthentificationRequest;
 import org.example.userservice.entity.UserEntity;
+import org.example.userservice.service.AuthentificationService;
 import org.example.userservice.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthentificationController {
     private final UserService userService;
+    private final AuthentificationService authentificationService;
 
-    public AuthentificationController(UserService userService) {
+    public AuthentificationController(UserService userService, AuthentificationService authentificationService) {
         this.userService = userService;
+        this.authentificationService = authentificationService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserEntity> registerUser(@RequestParam String username, @RequestParam String password) {
-        return ResponseEntity.ok(userService.registerUser(username, password));
+    public ResponseEntity<UserEntity> registerUser(@RequestBody AuthentificationRequest request) {
+        return ResponseEntity.ok(userService.registerUser(request.getUsername(), request.getPassword()));
     }
 
     @PostMapping("/register-admin")
-    public ResponseEntity<UserEntity> registerAdmin(@RequestParam String username, @RequestParam String password) {
-        return ResponseEntity.ok(userService.registerAdmin(username, password));
+    public ResponseEntity<UserEntity> registerAdmin(@RequestBody AuthentificationRequest request) {
+        return ResponseEntity.ok(userService.registerAdmin(request.getUsername(), request.getPassword()));
     }
-}
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthentificationRequest request) {
+        return ResponseEntity.ok(authentificationService.authenticate(request));
+    }}
