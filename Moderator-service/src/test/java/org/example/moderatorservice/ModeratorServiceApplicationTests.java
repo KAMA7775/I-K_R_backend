262215cd83bc13ea5e1f1cec992e2f8cdf413547
KAMA7775 <kalia.abdusamatova@gmail.com>
2025-04-23 13:13:18 +0600

    added moderator panel

---
+++ Moderator-service/src/test/java/org/example/moderatorservice/ModeratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moderatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeratorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
